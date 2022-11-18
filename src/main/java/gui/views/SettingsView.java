f08c0d908c9572a9ede7f2f768f9d7b67328b126
Talu <talupark@Talulahs-MacBook-Pro.local>
2022-11-18 11:58:01 +0100

    une fois la taille de la raquette est choisie le jeu ne lance pas directement. l'information est stockee pour le prochain lancement du jeu

--- src/main/java/gui/views/SettingsView.java
+++ src/main/java/gui/views/SettingsView.java
@@ -64,25 +64,28 @@ public class SettingsView extends View{ // Classe similaire à GameView.java & M
         tailleRaquette.setText("Racket Size");
 
         raquetteSmall = new Button();
+        raquetteMedium = new Button();
+        raquetteLarge = new Button();
+
         raquetteSmall.setText("Small");
         raquetteSmall.setLayoutX(((court.getWidth() / 2) * scale) - 80);
         raquetteSmall.setLayoutY(250);
         raquetteSmall.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        raquetteSmall.setOnAction(event -> {MenuView.activateSmall(); sceneHandler.switchToGameR(getRoot(), 75.0); makePauseFalse(); });
+        raquetteSmall.setOnAction(event -> {MenuView.activateSmall(); makePauseFalse(); raquetteSmall.setStyle("-fx-background-color: #FDFCCB"); raquetteMedium.setStyle(null); raquetteLarge.setStyle(null);});
 
-        raquetteMedium = new Button();
+
         raquetteMedium.setText("Medium");
         raquetteMedium.setLayoutX(((court.getWidth() / 2) * scale) + 11);
         raquetteMedium.setLayoutY(250);
         raquetteMedium.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        raquetteMedium.setOnAction(event -> {MenuView.activateMedium();sceneHandler.switchToGameR(getRoot(), 100.0); makePauseFalse(); });
+        raquetteMedium.setOnAction(event -> {MenuView.activateMedium(); makePauseFalse(); raquetteMedium.setStyle("-fx-background-color: #FDFCCB"); raquetteSmall.setStyle(null); raquetteLarge.setStyle(null);});
 
-        raquetteLarge = new Button();
+
         raquetteLarge.setText("Large");
         raquetteLarge.setLayoutX(((court.getWidth() / 2) * scale) + 120);
         raquetteLarge.setLayoutY(250);
         raquetteLarge.setFont(Font.font("verdana", FontWeight.BOLD, FontPosture.REGULAR, 15));
-        raquetteLarge.setOnAction(event -> {MenuView.activateLarge(); sceneHandler.switchToGameR(getRoot(), 150.0); makePauseFalse(); raquetteLarge.setStyle("-fx-background-color: #00ff00"); });
+        raquetteLarge.setOnAction(event -> {MenuView.activateLarge(); makePauseFalse(); raquetteLarge.setStyle("-fx-background-color: #FDFCCB"); raquetteSmall.setStyle(null); raquetteMedium.setStyle(null);});
 
         //on change la coleur du bouton qui change la taille de la raquette pour que ca soit plus clair quelle taille est actuellement choisie
 
